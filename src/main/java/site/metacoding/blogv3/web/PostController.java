e5c8ab8354667026b57f283fb3f67bc829240646
field <jsw_777@naver.com>
2022-04-18 12:24:42 +0900

    카테고리 목록 보기 완료

--- src/main/java/site/metacoding/blogv3/web/PostController.java
+++ src/main/java/site/metacoding/blogv3/web/PostController.java
@@ -2,18 +2,28 @@ package site.metacoding.blogv3.web;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.config.auth.LoginUser;
+import site.metacoding.blogv3.service.PostService;
+import site.metacoding.blogv3.web.dto.post.PostRespDto;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
     @GetMapping("/user/{id}/post")
-    public String postList(@PathVariable Integer id, @AuthenticationPrincipal LoginUser loginUser) {
+    public String postList(@PathVariable Integer id, @AuthenticationPrincipal LoginUser loginUser, Model model) {
+
+        // SELECT * FROM category WHERE userId = :id
+        // 카테고리 가져갑시다
+        PostRespDto postRespDto = postService.게시글목록보기(id);
+        model.addAttribute("postRespDto", postRespDto);
 
         return "/post/list";
     }
